72dbf52a5a75661febc4663db3b211a299f16543
krostyslav <rostyslav.kryvyy@gmail.com>
2017-07-09 21:35:28 +0300

    first commmit

---
+++ server.js
@@ -0,0 +1,69 @@
+/**
+ * Created by krostyslav on 12.06.17.
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var artists = [
+    {
+        id: 1,
+        name: 'Metalica'
+    },
+    {
+        id: 2,
+        name: 'Iron Maiden'
+    },
+    {
+        id: 3,
+        name: 'Deep Purple'
+    }
+];
+
+app.get('/', function (req, res) {
+    res.send('Hello API');
+});
+
+app.get('/artists', function (req, res) {
+    res.send(artists);
+});
+
+app.get('/artists/:id', function (req, res) {
+    var artist = artists.find(function (artist) {
+        return artist.id === Number(req.params.id)
+    });
+    res.send(artist);
+});
+
+app.post('/artists', function (req, res) {
+    var artist = {
+        id: Date.now(),
+        name: req.body.name
+    };
+    artists.push(artist);
+    console.log(req.params);
+    res.send(artist);
+});
+
+app.put('/artists/:id', function (req, res) {
+    var artist = artists.find(function (artist) {
+        return artist.id === Number(req.params.id)
+    });
+    artist.name = req.body.name;
+    res.sendStatus(200);
+});
+
+app.delete('/artists/:id', function (req, res) {
+    artists = artists.filter(function (artist) {
+        return artist.id !== Number(req.params.id)
+    });
+    res.sendStatus(200);
+});
+
+app.listen(3012, function () {
+    console.log('API app started');
+});
