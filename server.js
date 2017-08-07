323fd0980035e59a09525361a4be83a639c5280f
krostyslav <rostyslav.kryvyy@gmail.com>
2017-08-07 21:37:47 +0300

    update fake apu

--- server.js
+++ server.js
@@ -1,33 +1,13 @@
 /**
- * Created by krostyslav on 12.06.17.
+ * Created by krostyslav
  */
-const config = {
-    username: 'kxmmvragqrygfi',
-    password: '39c358751cc54f72c3e1f9ad14a50cfbeaa5daa4ebc3d079baa584e1164b74a5',
-    host: 'ec2-23-21-246-11.compute-1.amazonaws.com',
-    port: '5432',
-    database: 'dfr4velvir2hae'
-};
-
-const URI = 'postgres://kxmmvragqrygfi:39c358751cc54f72c3e1f9ad14a50cfbeaa5daa4ebc3d079baa584e1164b74a5@ec2-23-21-246-11.compute-1.amazonaws.com:5432/dfr4velvir2hae';
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var pg = require('pg');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
-
-// app.get('/db', function (request, response) {
-//     pg.connect(process.env.URI, function(err, client, done) {
-//         client.query('SELECT * FROM test_table', function(err, result) {
-//             done();
-//             if (err)
-//             { console.error(err); response.send("Error " + err); }
-//             else
-//             { response.render('pages/db', {results: result.rows} ); }
-//         });
-//     });
-// });
+var db;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -39,16 +19,24 @@ var artists = [
     },
     {
         id: 2,
-        name: 'Iron Maiden'
+        name: 'Nirvana'
     },
     {
         id: 3,
         name: 'Deep Purple'
+    },
+    {
+        id: 4,
+        name: 'R.E.M.'
+    },
+    {
+        id: 5,
+        name: 'Iron Maiden'
     }
 ];
 
 app.get('/', function (req, res) {
-    res.send('Hello API');
+    res.send('Hello Fake API');
 });
 
 app.get('/artists', function (req, res) {
@@ -87,10 +75,16 @@ app.delete('/artists/:id', function (req, res) {
     res.sendStatus(200);
 });
 
-// app.listen(3012, function () {
-//     console.log('API app started');
-// });
-
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(3012, function () {
+    console.log('Fake API app started');
 });
+
+// MongoClient.connect('mongodb://localhost:27017/fakeApi', function (err, database) {
+//     if (err) {
+//         return console.log(err);
+//     }
+//     db = database;
+//     app.listen(process.env.PORT || 3012, function () {
+//         console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//     });
+// });
