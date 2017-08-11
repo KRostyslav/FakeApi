5eb3479cd52a08f43ac02b36d256ec648b9a2c20
KRostyslav <rostyslav.kryvyy@gmail.com>
2017-08-11 13:27:03 +0300

    added header

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
@@ -134,6 +135,26 @@ app.delete('/artists/:id', function (req, res) {
         return artist.id !== Number(req.params.id)
     });
     res.sendStatus(200);
+});
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 
 app.listen(process.env.PORT || 3012, function () {
